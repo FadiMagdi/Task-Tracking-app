db12846944c43c1118d4a0f1c8716c387e139a76
FadiMagdi <fadimagdi97@yahoo.com>
2025-03-31 20:14:25 +0200

    finished project

--- src/main/java/com/FadiMagdi/alternative/task/app/services/Impl/TaskServiceImpl.java
+++ src/main/java/com/FadiMagdi/alternative/task/app/services/Impl/TaskServiceImpl.java
@@ -7,11 +7,13 @@ import com.FadiMagdi.alternative.task.app.domain.entities.TaskStatus;
 import com.FadiMagdi.alternative.task.app.repositories.TaskListRepository;
 import com.FadiMagdi.alternative.task.app.repositories.TaskRepository;
 import com.FadiMagdi.alternative.task.app.services.TaskService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -31,6 +33,8 @@ private final TaskListRepository taskListRepository;
     public List<Task> listTasks(UUID taskListID) {
         return this.taskRepository.findByTaskListId(taskListID) ;
     }
+
+    @Transactional
 
     @Override
     public Task createTask(UUID taskListId, Task task) {
@@ -72,5 +76,48 @@ return this.taskRepository.save(taskToSave);
     @Override
     public Optional<Task> getTask(UUID taskListId, UUID taskId) {
         return taskRepository.findByTaskListIdAndId(taskListId,taskId);
+    }
+
+    @Override
+    public Task updateTask(UUID TaskListID, UUID taskID, Task task) {
+
+        if(task.getId() == null){
+            throw new IllegalArgumentException("Task must have an ID");
+        }
+
+        if(!Objects.equals(taskID , task.getId())){
+            throw new IllegalArgumentException("IDs don't match");
+        }
+
+
+        if(task.getPriority() == null){
+            throw new IllegalArgumentException("task should have valid priority");
+        }
+
+        if(task.getStatus() == null){
+            throw new IllegalArgumentException("task should have a status");
+        }
+
+        Task existingTask = this.taskRepository.findByTaskListIdAndId(TaskListID,task.getId())
+                .orElseThrow(()->new IllegalArgumentException("task not found"));
+
+
+
+        existingTask.setTitle(task.getTitle());
+  existingTask.setDueDate(task.getDueDate());
+  existingTask.setDescription(task.getDescription());
+existingTask.setPriority(task.getPriority());
+existingTask.setStatus(task.getStatus());
+existingTask.setUpdated(LocalDateTime.now());
+
+return this.taskRepository.save(existingTask);
+
+    }
+@Transactional // added because the custom method need to be transactional for database consistency
+    @Override
+    public void deleteTask(UUID taskListId, UUID taskId) {
+this.taskRepository.deleteByTaskListIDandTaskId(taskListId,taskId);
+
+
     }
 }
